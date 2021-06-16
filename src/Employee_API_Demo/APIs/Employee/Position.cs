5c79d9436007fc31313614c8f4b1cf8490940ab1
Thomas Skjørten <Thomas.Skjorten@visma.com>
2021-06-16 21:14:02 +0200

    Cleanup

--- src/Employee_API_Demo/APIs/Employee/Position.cs
+++ src/Employee_API_Demo/APIs/Employee/Position.cs
@@ -1,11 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HRM_API_Demo.APIs.Employee
+﻿namespace HRM_API_Demo.APIs.Employee
 {
     public class Position
     {
