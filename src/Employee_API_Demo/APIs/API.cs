9903e2a9e7d283d3ae7c89c6377eaeb94417334a
Thomas Skjørten <Thomas.Skjorten@visma.com>
2021-06-17 08:16:30 +0200

    Added comments

--- src/Employee_API_Demo/APIs/API.cs
+++ src/Employee_API_Demo/APIs/API.cs
@@ -7,21 +7,33 @@ using System.Threading.Tasks;
 
 namespace HRM_API_Demo.APIs
 {
+    /// <summary>
+    /// Base-class for API-wrapper classes. Since all HRM-APIs uses Visma Connect for authentication the logic for getting the token is placed here
+    /// </summary>
     public abstract class API
     {
+        /// <summary>
+        /// All API-wrapper classes must contain information about the API-Scopes they require
+        /// </summary>
+        /// <returns>A string including the required scopes (separated by [space])</returns>
         protected abstract string GetRequiredScopes();
 
+        /// <summary>
+        /// Collects a new token from Visma Connect.
+        /// </summary>
+        /// <returns>The token</returns>
         protected async Task<string> GetToken()
         {
             using (var client = new HttpClient())
             {
                 using (var request = new HttpRequestMessage(HttpMethod.Post, "https://connect.visma.com/connect/token"))
                 {
+                    // The token-request requires the grant_type (always client_credentials), the ClientId/Secret a tenantId and the scopes to be provided as FormUrlEncoded content in the request. 
                     request.Content = new FormUrlEncodedContent(new Dictionary<string, string> {
                         { "grant_type", "client_credentials" },
                         { "client_id", Environment.GetEnvironmentVariable("ClientId")},
                         { "client_secret", Environment.GetEnvironmentVariable("ClientSecret") },
-                        { "tenant_id", Environment.GetEnvironmentVariable("TenantId") },
+                        { "tenant_id", Environment.GetEnvironmentVariable("TenantId") }, // Since the APIs are tenant-enabled, a valid tenantId must be provided to obtain a token.
                         { "scope", GetRequiredScopes() }
                     });
 
@@ -31,7 +43,10 @@ namespace HRM_API_Demo.APIs
                     var payload = Newtonsoft.Json.Linq.JObject.Parse(await response.Content.ReadAsStringAsync());
                     var token = payload.Value<string>("access_token");
 
+                    // Returns the created token
                     return token;
+
+                    //NB! The token is valid for 3600 seconds (1 hour), so it should be reused for this time. This is not implemented in this test.
                 }
             }
         }
--- src/Employee_API_Demo/Program.cs
+++ src/Employee_API_Demo/Program.cs
@@ -1,10 +1,12 @@
 ﻿using HRM_API_Demo.APIs.Employee;
 using System;
-using System.Linq;
 using System.Threading;
 
 namespace HRM_API_Demo
 {
+    /// <summary>
+    /// Console-application to run the tests. 
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
@@ -12,6 +14,9 @@ namespace HRM_API_Demo
             ShowMenu();
         }
 
+        /// <summary>
+        /// Shows the main menu for the test-application
+        /// </summary>
         private static void ShowMenu()
         {
             Console.Clear();
@@ -54,23 +59,30 @@ namespace HRM_API_Demo
             }
         }
 
+        /// <summary>
+        /// Executes the test to get multiple Employees from the API
+        /// </summary>
         private static void ListEmployees()
         {
+            // Creates an instance of the EmployeeAPI-wrapper
             APIs.Employee.EmployeeAPI empAPI = new();
 
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Getting a list of employees");
 
+            // Makes the call to get a list of employees from the API. 
+            // The communication to the API is async, but in this case we want to wait for the task to finish before continuing.
             var emplist = empAPI.GetEmployees().GetAwaiter().GetResult();
 
             Console.ForegroundColor = ConsoleColor.White; Console.WriteLine();
             Console.WriteLine("Firstname      LastName       Date of Birth");
             Console.WriteLine("-------------- -------------- -------------");
 
+            // Loops through all the returned employees and print the result
             foreach (var employee in emplist)
             {
                 Console.WriteLine("{0}{1}{2}", 
-                    employee.personNames[0].firstName.PadRight(15, ' '), 
+                    employee.personNames[0].firstName.PadRight(15, ' '),  // An employee can have multiple names, for simplicity we choose the first one.
                     employee.personNames[0].lastName.PadRight(15, ' '), 
                     employee.dateOfBirth);
             }
@@ -84,24 +96,37 @@ namespace HRM_API_Demo
             ShowMenu();
         }
 
+        /// <summary>
+        /// Executes the test to get a single employee, including the positions for this employee from the API
+        /// </summary>
         private static void GetEmployee()
         {
+            // Creates an instance of the EmployeeAPI-wrapper
             APIs.Employee.EmployeeAPI empAPI = new();
 
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Getting a single employee based on Id and listing all positions");
 
+            // Makes the call to get a single of employees from the API. For simplicity we use a hardcoded EmployeeID. 
+            // The communication to the API is async, but in this case we want to wait for the task to finish before continuing.
             var singleemployee = empAPI.GetEmployee("d032f491-e834-4844-914c-f03c0d82e278").GetAwaiter().GetResult();
+
+            // Makes the call to get a list of all the positions for the employees from the API. For simplicity we use a hardcoded EmployeeID. 
+            // The communication to the API is async, but in this case we want to wait for the task to finish before continuing.
             var positions = empAPI.GetPositions("d032f491-e834-4844-914c-f03c0d82e278").GetAwaiter().GetResult();
 
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine();
+
+            // Writes out the name of the employee. An employee can have multiple names, for simplicity we choose the first one.
             Console.WriteLine("Employee: {0} {1}", singleemployee.personNames[0].firstName, singleemployee.personNames[0].lastName);
 
             Console.WriteLine();
             Console.WriteLine("Type of position Employmentform Start date Salarytype Salary");
             Console.WriteLine("---------------- -------------- ---------- ---------- ------");
 
+            // Loops through all the returned positions and print the results.
+            // For the properties where there can be more than result (empolymentform, salaryinformation) we choose the first one for simplicity.
             foreach (var pos in positions)
             {
                 Console.WriteLine("{0}{1}{2}{3}{4}", 
@@ -122,10 +147,13 @@ namespace HRM_API_Demo
             ShowMenu();
         }
 
+        /// <summary>
+        /// Executes the test to create a new employee with a position via the API.
+        /// </summary>
+        /// <param name="includeTestData">Creating an employee with a position have some required fields, set this parameter to TRUE to insert testdata for these fields.</param>
         private static void CreateEmployee(bool includeTestData)
         {
-            Console.Clear();
-
+            // Creates an instance of the EmployeeAPI-wrapper
             APIs.Employee.EmployeeAPI empAPI = new();
 
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -134,22 +162,24 @@ namespace HRM_API_Demo
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Employeenumber: ");
-            var employeeNumber = Console.ReadLine();
+            var employeeNumber = Console.ReadLine(); // Collects the employeenumber from the user.
 
             Console.Write("Firstname: ");
-            var firstname = Console.ReadLine();
+            var firstname = Console.ReadLine(); // Collects the employeenumber from the user.
 
             Console.Write("Lastname: ");
-            var lastname = Console.ReadLine();
+            var lastname = Console.ReadLine(); // Collects the employeenumber from the user.
 
             Console.WriteLine();
 
             Console.WriteLine("Creating {0} {1}...", firstname, lastname);
 
+            // Creates an instance of the EmployeeData-class. This class is generated by Visual Studio based on the Swagger-documentation for the API.
             EmployeeData employeeData = new EmployeeData();
-            employeeData.personNames = new[] { new Personname() { firstName = firstname, lastName = lastname } };
-            employeeData.number = employeeNumber;
+            employeeData.personNames = new[] { new Personname() { firstName = firstname, lastName = lastname } }; // Creates the name-instance.
+            employeeData.number = employeeNumber; // Inserts the employee-number.
 
+            // If the option to insert static test-data has been chosen, we append data in the required fields
             if(includeTestData)
             { 
                 employeeData.printPayslip = false;
@@ -167,25 +197,33 @@ namespace HRM_API_Demo
                                                  };
             }
 
+            // Makes the call to create the employee via the API. 
+            // The communication to the API is async, but in this case we want to wait for the task to finish before continuing.
+            // The task of creating a new employee is run asynchronous on the server-side. Therefore the returned value is a path to check the status of the creation-task.
             var jobpath = empAPI.CreateEmployee(employeeData).GetAwaiter().GetResult();
 
             Console.WriteLine("Job created. Waiting for task to finish...");
 
             Job job = null;
 
+            // While waiting for the job (create new employee) to be completed by the server we make calls to the server every 3 seconds to check the status of the job.
             while (job == null || job.status == "NotStarted" || job.status == "InProgress")
             {
+                // Calls the API to get the status of the job.
+                // The communication to the API is async, but in this case we want to wait for the task to finish before continuing.
                 job = empAPI.GetJob(jobpath).GetAwaiter().GetResult();
                 Thread.Sleep(3000);
             }
 
+            // If the job completes with a "Failed"-status we write out the error-messages to the screen.
             if(job.status == "Failed") 
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Job failed: {0}", job.message);
                 Console.WriteLine();
             }
-            else if(job.status == "Succeeded")
+            // If the job completes with a "Succeeded"-status we write out a message to the screen.
+            else if (job.status == "Succeeded")
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Employee created");      
